f9f4648fe7f9bc3d1a3f50a39a34091f932c4e17
Chunlee Thong <44610456+Chunlee17@users.noreply.github.com>
2020-05-01 22:45:11 +0700

    Update telegram_sliver.dart

--- lib/playground_pages/telegram_sliver.dart
+++ lib/playground_pages/telegram_sliver.dart
@@ -8,8 +8,7 @@ class TelegramSliver extends StatefulWidget {
   _TelegramSliverState createState() => _TelegramSliverState();
 }
 
-class _TelegramSliverState extends State<TelegramSliver>
-    with SingleTickerProviderStateMixin {
+class _TelegramSliverState extends State<TelegramSliver> with SingleTickerProviderStateMixin {
   double maxSliverHeight = 200;
   double minimumShowIconHeight = 140;
   double height;
@@ -35,80 +34,82 @@ class _TelegramSliverState extends State<TelegramSliver>
     var safePadding = MediaQuery.of(context).padding.top;
     print("Rebuild page");
     return Scaffold(
-      body: NestedScrollView(
-        controller: scrollController,
-        headerSliverBuilder: (context, scrolled) => [
-          SliverSafeArea(
-            top: false,
-            sliver: SliverAppBar(
-              expandedHeight: maxSliverHeight,
-              pinned: true,
-              snap: false,
-              floating: false,
-              bottom: sliverBottomWidget(),
-              actions: <Widget>[
-                IconButton(icon: Icon(Icons.call), onPressed: () {}),
-                IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
-              ],
-              flexibleSpace: LayoutBuilder(builder: (context, constraint) {
-                height = constraint.biggest.height;
-                heightController.add(height);
-                return FlexibleSpaceBar(
-                  collapseMode: CollapseMode.pin,
-                  stretchModes: [StretchMode.fadeTitle],
-                  titlePadding: EdgeInsets.only(
-                      left:
-                          (1 - height / (maxSliverHeight + safePadding)).abs() *
-                              80),
-                  title: Stack(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              CircleAvatar(
-                                child: Text("C"),
-                                backgroundColor: Colors.white,
-                              ),
-                              UIHelper.horizontalSpace(),
-                              Column(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text("Chunlee Thong"),
-                                  Text(
-                                    "last seen recenlty",
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.normal),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: maxSliverHeight,
+            pinned: true,
+            stretch: true,
+            stretchTriggerOffset: 100,
+            onStretchTrigger: () {
+              print("Stretch");
+              return;
+            },
+            snap: false,
+            floating: false,
+            bottom: sliverBottomWidget(),
+            actions: <Widget>[
+              IconButton(icon: Icon(Icons.call), onPressed: () {}),
+              IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+            ],
+            flexibleSpace: LayoutBuilder(builder: (context, constraint) {
+              height = constraint.biggest.height;
+              heightController.add(height);
+              return FlexibleSpaceBar(
+                collapseMode: CollapseMode.parallax,
+                stretchModes: [StretchMode.zoomBackground],
+                background: Image.network(
+                  "https://picsum.photos/200",
+                  fit: BoxFit.cover,
+                ),
+                titlePadding: EdgeInsets.only(left: (1 - height / (maxSliverHeight + safePadding)).abs() * 80),
+                title: Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            CircleAvatar(
+                              child: Text("C"),
+                              backgroundColor: Colors.white,
+                            ),
+                            UIHelper.horizontalSpace(),
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text("Chunlee Thong"),
+                                Text(
+                                  "last seen recenlty",
+                                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal),
+                                )
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
-                );
-              }),
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(top: 32),
+            sliver: SliverGrid.count(
+              crossAxisCount: 2,
+              children: List.generate(
+                20,
+                (index) => Card(
+                  child: Center(child: Text("Hello World")),
+                ),
+              ),
             ),
           )
         ],
-        body: ListView.separated(
-          itemCount: 15,
-          separatorBuilder: (context, index) => Divider(height: 0),
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text("Hello"),
-              leading: CircleAvatar(child: Text('T')),
-              onTap: () {},
-            );
-          },
-        ),
       ),
     );
   }
