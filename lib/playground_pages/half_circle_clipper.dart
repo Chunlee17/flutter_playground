d3a8e29a7f0c673a07e33cd16d44009bc80a01d4
chunleethong <thong.chunlee@optistech.com>
2020-05-08 15:42:46 +0700

    Update half_circle_clipper.dart

--- lib/playground_pages/half_circle_clipper.dart
+++ lib/playground_pages/half_circle_clipper.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:jin_widget_helper/jin_widget_helper.dart';
 
 class HalfCircleClipper extends StatefulWidget {
   @override
@@ -16,92 +17,152 @@ class _HalfCircleClipperState extends State<HalfCircleClipper> {
         title: Text("Haft Circle Clipper"),
       ),
       body: Center(
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: MediaQuery.of(context).size.width / 2,
-                color: Colors.cyan,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 44),
-                      child: CircleAvatar(
-                        child: Icon(Icons.credit_card, color: Colors.white),
-                        backgroundColor: Colors.pink,
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: MediaQuery.of(context).size.width / 2,
+                    color: Colors.cyan,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 44),
+                          child: CircleAvatar(
+                            child: Icon(Icons.credit_card, color: Colors.white),
+                            backgroundColor: Colors.pink,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(10, (index) {
+                              return Container(
+                                  width: 1, height: 6, color: Colors.white);
+                            }),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          "HELLO WORLD",
+                          style: TextStyle(fontSize: 24, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 54,
+                  child: ClipPath(
+                    clipper: CustomHalfCircleClipper(isTop: true),
+                    child: Container(
+                      height: circleRadius,
+                      width: circleRadius,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 54,
+                  child: ClipPath(
+                    clipper: CustomHalfCircleClipper(),
+                    child: Container(
+                      height: circleRadius,
+                      width: circleRadius,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: ClipPath(
+                    clipper: TopTagClipper(),
+                    child: Container(
+                      height: circleRadius * 3,
+                      width: circleRadius * 3,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(16)),
+                        gradient: LinearGradient(
+                          colors: [Colors.yellow, Colors.green],
+                        ),
+                        //color: Colors.red,
+                      ),
+                      alignment: Alignment.topRight,
+                      padding: EdgeInsets.only(top: 12, right: 8),
+                      child: Transform.rotate(
+                        angle: pi / 4,
+                        child: Text(
+                          "15%",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: List.generate(10, (index) {
-                          return Container(width: 1, height: 6, color: Colors.white);
-                        }),
-                      ),
-                    ),
-                    SizedBox(width: 16),
-                    Text(
-                      "HELLO WORLD",
-                      style: TextStyle(fontSize: 24, color: Colors.white),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-            Positioned(
-              top: 0,
-              left: 54,
-              child: ClipPath(
-                clipper: CustomHalfCircleClipper(isTop: true),
-                child: Container(
-                  height: circleRadius,
-                  width: circleRadius,
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                ),
+            UIHelper.verticalSpace(24),
+            FoodCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget FoodCard() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.width * 0.55,
+        decoration: BoxDecoration(color: Color(0xFF06B79D)),
+        child: Row(
+          children: <Widget>[
+            ClipPath(
+              clipper: FoodClipper(),
+              child: Image.network(
+                "https://picsum.photos/500",
+                fit: BoxFit.cover,
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 54,
-              child: ClipPath(
-                clipper: CustomHalfCircleClipper(),
-                child: Container(
-                  height: circleRadius,
-                  width: circleRadius,
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: ClipPath(
-                clipper: TopTagClipper(),
-                child: Container(
-                  height: circleRadius * 3,
-                  width: circleRadius * 3,
+            ).expanded,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.watch_later, color: Colors.white),
+                Text("20min", style: TextStyle(color: Colors.white)),
+                Divider(color: Colors.white, thickness: 1),
+                Icon(Icons.favorite, color: Colors.white),
+                Text("130cal", style: TextStyle(color: Colors.white)),
+                Divider(color: Colors.white, thickness: 1),
+                Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topRight: Radius.circular(16)),
-                    gradient: LinearGradient(
-                      colors: [Colors.yellow, Colors.green],
-                    ),
-                    //color: Colors.red,
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  alignment: Alignment.topRight,
-                  padding: EdgeInsets.only(top: 12, right: 8),
-                  child: Transform.rotate(
-                    angle: pi / 4,
-                    child: Text(
-                      "15%",
-                      style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.shopping_cart),
+                      Text("3/6"),
+                    ],
                   ),
                 ),
-              ),
-            ),
+              ],
+            ).padding(EdgeInsets.symmetric(horizontal: 16, vertical: 16))
           ],
         ),
       ),
@@ -134,6 +195,23 @@ class TopTagClipper extends CustomClipper<Path> {
     final Path path = new Path();
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class FoodClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width * 0.9, size.height);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width * 0.9, 0);
     return path;
   }
 
