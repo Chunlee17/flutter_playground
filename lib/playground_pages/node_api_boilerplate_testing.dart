55829d93698fe3c747c0d2ea014e549d32e2d06e
chunleethong <thong.chunlee@optistech.com>
2020-06-23 14:27:43 +0700

    Update node_api_boilerplate_testing.dart

--- lib/playground_pages/node_api_boilerplate_testing.dart
+++ lib/playground_pages/node_api_boilerplate_testing.dart
@@ -15,28 +15,36 @@ class _NodeApiBoilerPlateTestingState extends State<NodeApiBoilerPlateTesting> {
   NodeApiBoilerPlateApi nodeApiBoilerPlate = NodeApiBoilerPlateApi();
   BehaviorSubject<String> loginController = BehaviorSubject();
   BehaviorSubject<String> userController = BehaviorSubject();
+  var loginLoading = false.obs<bool>();
+  var getInfoLoading = false.obs<bool>();
 
   Future onLoginUser() async {
+    loginLoading.value = true;
     nodeApiBoilerPlate.loginUser().then(
       (response) {
+        loginLoading.value = false;
         nodeApiBoilerPlate.token = response['token'];
         nodeApiBoilerPlate.userId = response['data']['_id'];
         loginController.add(response.toString());
       },
     ).catchError(
       (error) {
+        loginLoading.value = false;
         loginController.addError(error.toString());
       },
     );
   }
 
   Future fetchUserInfo() async {
+    getInfoLoading.value = true;
     nodeApiBoilerPlate.getUserInfo().then(
       (response) {
         userController.add(response.toString());
+        getInfoLoading.value = false;
       },
     ).catchError(
       (error) {
+        getInfoLoading.value = false;
         userController.addError(error.toString());
       },
     );
@@ -55,30 +63,33 @@ class _NodeApiBoilerPlateTestingState extends State<NodeApiBoilerPlateTesting> {
       appBar: AppBar(
         title: Text("Node Api Boilerplate"),
       ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           ActionButton(
             child: Text("Login User"),
             onPressed: onLoginUser,
+            isLoading: loginLoading,
           ),
           StreamHandler(
             stream: loginController.stream,
             initialData: "Not login yet",
             error: (error) => Text(
-              "Error: ${error.toString()}",
+              error.toString(),
               style: TextStyle(color: Colors.red),
             ),
             ready: (data) => Text(data.toString()),
           ),
           ActionButton(
             child: Text("Get User"),
+            isLoading: getInfoLoading,
             onPressed: fetchUserInfo,
           ),
           StreamHandler(
             stream: userController.stream,
             error: (error) => Text(
-              "Error: ${error.toString()}",
+              error.toString(),
               style: TextStyle(color: Colors.red),
             ),
             initialData: "Login user first",
