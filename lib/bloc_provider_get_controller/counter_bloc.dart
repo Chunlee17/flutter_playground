7ab46543d43ba7058540b915762ca39c88df2a1e
chunleethong <thong.chunlee@optistech.com>
2020-07-02 16:56:17 +0700

    Update counter_bloc.dart

--- lib/bloc_provider_get_controller/counter_bloc.dart
+++ lib/bloc_provider_get_controller/counter_bloc.dart
@@ -2,7 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum CounterEvent { increment, decrement }
 
-class CounterBloc extends Bloc<CounterEvent, dynamic> {
+class CounterBloc extends Bloc<CounterEvent, int> {
   @override
   int get initialState => 0;
 
